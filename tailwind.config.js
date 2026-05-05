70a8119aa73704c46512eb2bba704b0b1fa8e890
UrbinaDan <dfu643@ensign.edu>
2026-05-05 15:20:11 -0600

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,10 @@
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,jsx}',
+    './components/**/*.{js,jsx}',
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: '#a21caf', // purple-700
-        secondary: '#f59e42', // yellow-400
-        accent: '#ec4899', // pink-500
-      },
-    },
+    extend: {},
   },
   plugins: [],
-}; 
+};
